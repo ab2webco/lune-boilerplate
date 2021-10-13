0b0b05cd2968f7a7007b4485ae2434b3219ba5bb
fabian ALtahona <fabian.altahona@koombea.com>
2021-10-13 16:40:03 -0500

    feat(Root): Update theme files

--- wp-content/themes/nix/footer.php
+++ wp-content/themes/nix/footer.php
@@ -1,4 +1,14 @@
 <?php
-	// Exit if accessed directly.
-	defined( 'ABSPATH' ) || exit;
-	wp_footer();
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit; ?>
+		</main>
+		<!-- Add your custom footer -->
+		<footer class="bg-light text-center text-lg-start">
+			<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+			© <span class="js-current-year">2021</span> Powered by:
+			<a class="text-dark" href="https://www.ab2web.com/" target="_blank">Ab2web</a>
+			</div>
+		</footer>
+		<?php wp_footer();?>
+	</body>
+</html>
